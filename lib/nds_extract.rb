fcbeb9804c0fe6eac5e3f081d0c1d0e01094b9c7
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 02:07:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,13 +11,7 @@ def directors_totals(nds)
   result = {}
   nil
 
-  director_index = 0
-  while director_index < nds.length do
-    current_director = nds[director_index][:name]
-    result[current_director] = gross_for_director( nds[director_index] )
-    director_index += 1
-  end
-  result
+ 
 end
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
