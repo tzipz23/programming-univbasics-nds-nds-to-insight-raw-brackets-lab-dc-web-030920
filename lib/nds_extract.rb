3c5cd22b883b9e6ace7020a2186884397eaf27f5
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 02:07:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,6 +11,15 @@ def directors_totals(nds)
   result = {}
   nil
 
+  director_index = 0
+  while director_index < nds.length do
+    current_director = nds[director_index][:name]
+    result[current_director] = gross_for_director( nds[director_index] )
+    director_index += 1
+  end
+  result
+end
+
  
 end
   #
