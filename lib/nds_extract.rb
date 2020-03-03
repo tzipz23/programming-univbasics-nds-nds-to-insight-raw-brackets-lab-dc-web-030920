43935110993beae743773580f58d4995759bb0c0
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 02:06:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,18 @@ end
   #
   #
   # Be sure to return the result at the end!
- 
+  
+ def gross_for_director(director_data)
+  movie_index = 0
+  movies = director_data[:movies]
+  total_gross_for_director = 0
+
+  while movie_index < movies.length do
+    total_gross_for_director += movies[movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+  total_gross_for_director
+end
 
 
   nil
