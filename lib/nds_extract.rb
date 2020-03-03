8833178ffb2ebf4bccf24b591f3170ef51451347
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 02:02:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,8 +9,7 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   } 
-  
-  nil
+
 
   director_index = 0
   while director_index < nds.length do
